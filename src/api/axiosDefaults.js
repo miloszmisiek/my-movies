597521d871da5bcf673b073625a0cd9b3b9001af
miloszmisiek <84859537+miloszmisiek@users.noreply.github.com>
2022-12-01 18:10:32 +0100

    Change baseURL in axiosDefaults.js

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -4,7 +4,7 @@ const DEV = false;
 const devURL = "https://localhost:7151/";
 const prodURL = "https://mymovies-task.azurewebsites.net/";
 
-DEV ? (axios.defaults.baseURL = devURL) : prodURL;
+axios.defaults.baseURL = DEV ? devURL : prodURL;
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 
 export const axiosReq = axios.create();
